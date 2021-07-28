98281f8fcd4465fc1e9073180c99cce482e829e0
Dream-991029 <981846339@qq.com>
2021-07-28 14:35:43 +0800

    '常用axios默认属性'

--- axios封装/src/main.js
+++ axios封装/src/main.js
@@ -13,9 +13,15 @@ new Vue({
   router,
   render: h => h(App)
 })
+
+// 常用默认属性
+// 默认全局url前缀
+axios.defaults.baseURL = 'http://123.207.32.32:8080'
+// 默认全局超时时间
+axios.defaults.timeout = 5000
 // 简单使用
 axios({
-  url: 'https://httpbin.org/get'
+  url: ''
   // params: {
   //   type: 'sell',
   //   page: 3
@@ -28,13 +34,15 @@ axios({
 // 网络并发
 axios.all([
   axios({
-    url: 'https://httpbin.org/get'
+    url: '/category'
   }),
   axios({
-    url: 'https://httpbin.org/get'
+    url: '/home'
     // method: 'post'
   })
 ]).then(axios.spread((res1, res2) => {
   console.log(res1)
   console.log(res2)
-}))
+})).catch(err => {
+  console.log(err)
+})
